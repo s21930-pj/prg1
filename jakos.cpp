9af9c1772f96cad95ab1e77e1532bc9a6a1e2043
Kacper Skubiszewski <s21930@pjwstk.edu.pl>
2019-10-19 16:34:16 +0200

    bo tak

--- jakos.cpp
+++ jakos.cpp
@@ -1,26 +1,38 @@
 #include <iostream>
-#include <random> 
+#include <random>
 
+void naive_sort(int const N, int an_array[])
+{
+	for (auto i = 0; i < N; ++i) {
+ 		auto the_smallest = an_array[i];
+		auto index = i;
+		for (auto j = i; j < N; ++j) {
+			if (an_array[j] <the_smallest) {
+		    		the_smallest = an_array[j];
+		    		index = j;
+			}
+		}
+	an_array[index] = an_array[i];
+	an_array[i] = the_smallest;
+	}
+}
+void random_initialisation(int const N, int an_array[])
+{
+	std::random_device rd;
+	std::uniform_int_distribution<int> generator(0, 100);
+	for(auto i = 0; i < N; ++i)
+	 {
+		an_array[i] = generator(rd);
+	}
+}
 int main()
 {
-
 	auto const N = 10;
-
 	int an_array[N];
-
-	{
-
-		std::random_device rd;
-		std::uniform_int_distribution<int> generator (0, 100);
-		for (auto i = 0; i < N; ++i) {
-		    an_array[i] = generator(rd);
-		}
-	}
-
-	std::cout << "sizeof(an_array) : " << sizeof(an_array) << "\n";
+	random_initialisation(N, an_array);
+	naive_sort(N, an_array);
 	for (auto i = 0; i < N; ++i) {
-	    std::cout << an_array[i] << "\n";
+		std::cout << an_array[i] << "\n";
 	}
-
 	return 0;
 }
